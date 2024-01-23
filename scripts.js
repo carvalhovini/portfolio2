6899c2275fe85afeba6cabda6dac314c3a240aa1
Vinicius Carvalho Silva <68484993+carvalhovini@users.noreply.github.com>
2024-01-23 13:13:30 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,19 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const themeSwitch = document.getElementById('theme-toggle');
-
-    themeSwitch.addEventListener('click', function () {
-        document.body.classList.toggle('light-theme');
-        document.body.classList.toggle('dark-theme');
-        updateThemeButtonText();
-    });
-
-    function updateThemeButtonText() {
-        const themeButtonText = document.body.classList.contains('light-theme') ? 'White Mode' : 'Dark Mode';
-        themeSwitch.querySelector('.theme-text').textContent = themeButtonText;
-    }
-});
-
-document.addEventListener('DOMContentLoaded', function () {
     const sobreMimLink = document.querySelector('a[href="#sobre-mim"]');
 
     sobreMimLink.addEventListener('click', function (event) {
